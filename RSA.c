d826e507bc6de4c0dcc5184bf5224ad495552a4c
ShahriarAsfaq <55997420+ShahriarAsfaq@users.noreply.github.com>
2019-10-12 20:31:24 +0600

    Update RSA.c

--- RSA.c
+++ RSA.c
@@ -1,8 +1,32 @@
 #include<stdio.h>
 #include<ctype.h>
+#include<math.h>
 #include<stdlib.h>
 #include<string.h>
-int gcd(int a, int b)
+
+void main()
+{
+    int N,Q,e,d,p,q;
+    printf("Enter a prime no: ");
+    scanf("%d",&p);
+    printf("Enter another prime no: ");
+    scanf("%d",&q);
+    N=p*q;
+    Q=(p-1)*(q-1);
+    for(e=2;e<Q;e++){
+        if(fn(e,Q)==1)
+            break;
+    }
+    for(d=rand()%100000;;d++){
+        if(d!=1 && (d*e)%Q==1){
+            break;
+        }
+    }
+    encript(e,d,N);
+
+}
+
+int fn(int a, int b)
 {
 
     int x,y;
@@ -14,44 +38,21 @@ int gcd(int a, int b)
     return x;
 }
 
-
-void main()
-{
-    int N,Q,e,d,p,q;
-    printf("Enter first prime no: ");
-    scanf("%d",&p);
-    printf("Enter second prime no: ");
-    scanf("%d",&q);
-    N=p*q;
-    Q=(p-1)*(q-1);
-    for(e=2;e<Q;e++){
-        if(gcd(e,Q)==1)
-            break;
-    }
-    for(d=rand()%100000;;d++){
-        if(d!=1 && (d*e)%Q==1){
-            break;
-        }
-    }
-    crypto(e,d,N);
-
-}
-
-void crypto(int e, int d, int N)
+void encript(int e, int d, int N)
 {
     char a[1000];
     int p,i;
-    printf("Enter the message you want to encrypt:\n");
+    printf("Enter the normal message:\n");
     fflush(stdin);
     gets(a);
-    printf("Your encrypted message will be:\n");
+    printf("The encrypted message is:\n");
     for(i=0;i<strlen(a);i++){
         if(a[i]>='A' && a[i]<='Z'){
-          p=(int)pow((a[i]-'A'),e)%N;
+          p=(int)(pow((a[i]-'A'),e))%N;
           printf("%c",p+'A');
         }
         else if(a[i]>='a' && a[i]<='z'){
-            p=(int)pow((a[i]-'a'),e)%N;
+            p=(int)(pow((a[i]-'a'),e))%N;
             printf("%c",p+'a');
         }
         else
